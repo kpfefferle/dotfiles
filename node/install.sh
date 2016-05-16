58af7b7f41d472dc94336f1391ce4f9be8732905
Kevin Pfefferle <kevin@pfefferle.co>
2016-05-15 21:37:53 -0400

    Update node and ruby

--- node/install.sh
+++ node/install.sh
@@ -1,4 +1,19 @@
-if test ! $(which spoof)
+if test ! $(which bower)
 then
-  sudo npm install spoof -g
+  npm install -g bower
+fi
+
+if test ! $(which ember)
+then
+  npm install -g ember-cli
+fi
+
+if test ! $(which nombom)
+then
+  npm install -g nombom
+fi
+
+if test ! $(which phantomjs)
+then
+  npm install -g phantomjs-prebuilt
 fi
