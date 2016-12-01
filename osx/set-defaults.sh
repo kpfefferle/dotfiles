13aea4072fd838423f0bbcfe0cc99ebf2b79167d
Kevin Pfefferle <kevin@pfefferle.co>
2016-11-30 19:54:08 -0500

    Bunches of defaults

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -12,37 +12,62 @@ chflags nohidden ~/Library
 
 # Disable press-and-hold for keys in favor of key repeat.
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
-# Set a really fast key repeat.
-defaults write NSGlobalDomain KeyRepeat -int 0
+# Dark UI
+defaults write NSGlobalDomain AppleInterfaceStyle -string "Dark"
 # Disable reopen on restart
 defaults write NSGlobalDomain ApplePersistence -bool false
+# Finder: show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+# Set a really fast key repeat.
+defaults write NSGlobalDomain KeyRepeat -int 0
+# Disable 'smart' quotes
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+# Disable smart dashes
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
 
+# Autohide the Dock
+defaults write com.apple.dock autohide -int 1
 # Run the screensaver if we're in the bottom-left hot corner.
 defaults write com.apple.dock wvous-bl-corner -int 5
 defaults write com.apple.dock wvous-bl-modifier -int 0
 
+# Disable the warning when changing a file extension
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+# Always open everything in Finder's list view. This is important.
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
 # Set the Finder prefs for showing a few different volumes on the Desktop.
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+# Disable the warning before emptying the Trash
+defaults write com.apple.finder WarnOnEmptyTrash -bool false
 
-# Always open everything in Finder's list view. This is important.
-defaults write com.apple.Finder FXPreferredViewStyle Nlsv
+# Setup the menu bar date format
+defaults write com.apple.menuextra.clock DateFormat -string h:mm:ss a
+# Flash the : in the menu bar
+defaults write com.apple.menuextra.clock FlashDateSeparators -bool false
 
 # Use AirDrop over every interface. srsly this should be a default.
 defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
 
-# Dark UI
-defaults write NSGlobalDomain AppleInterfaceStyle -string "Dark"
-
-# Autohide the Dock
-defaults write com.apple.dock autohide -int 1
-
+# Don't fill passwords
+defaults write com.apple.Safari AutoFillPasswords -bool false
+defaults write com.apple.Safari AutoFillCreditCardData -int 0
 # Hide Safari's bookmark bar.
 defaults write com.apple.Safari ShowFavoritesBar -bool false
-
 # Set up Safari for development.
 defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
 defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+# Set screensaver time
+defaults -currentHost write com.apple.screensaver idleTime -int 600
+# Ask for password for screensaver
+defaults write com.apple.screensaver askForPassword -int 1
+# Set delay before password ask (minutes x 60)
+defaults write com.apple.screensaver askForPasswordDelay -int 5
+
+# Terminal theme
+defaults write com.apple.Terminal "Default Window Settings" -string "Pro"
+defaults write com.apple.Terminal "Startup Window Settings" -string "Pro"
