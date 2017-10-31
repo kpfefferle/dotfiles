f3c46fa1d6c3ab58f021aa64ddf03f7eb0d2fb4a
Kevin Pfefferle <kevin@pfefferle.co>
2017-10-31 12:25:52 -0400

    Update NPM global installs

--- node/install.sh
+++ node/install.sh
@@ -1,19 +1,9 @@
 if test ! $(which bower)
 then
-  yarn global add bower
+  npm install -g bower
 fi
 
 if test ! $(which ember)
 then
-  yarn global add ember-cli
-fi
-
-if test ! $(which nombom)
-then
-  yarn global add nombom
-fi
-
-if test ! $(which phantomjs)
-then
-  yarn global add phantomjs-prebuilt
+  npm install -g ember-cli
 fi
