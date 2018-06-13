43a14e6f5cd35c35f930665bbd0d9c34118c24ca
Kevin Pfefferle <kevin@pfefferle.co>
2018-06-13 12:31:06 -0400

    Temporarily remove brew doctor

--- homebrew/aliases.zsh
+++ homebrew/aliases.zsh
@@ -1,1 +1,2 @@
-alias brewup="brew update && brew doctor && brew outdated && brew upgrade && brew cleanup"
+# alias brewup="brew update && brew doctor && brew outdated && brew upgrade && brew cleanup"
+alias brewup="brew update && brew outdated && brew upgrade && brew cleanup"
