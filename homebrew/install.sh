7122cd0b143ba6882c3d23e0bda0a4fe3d0b150f
Kevin Pfefferle <kevin@pfefferle.me>
2021-10-27 21:47:23 -0400

    Add ohmyzsh install and temp disable further steps

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -9,7 +9,7 @@
 if test ! $(which brew)
 then
   echo "  Installing Homebrew for you."
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  /bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
 exit 0
